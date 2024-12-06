b68592937ef85f4882bd6c33c4d7d8e165713290
lewiii254 <ngondimarklewis@gmail.com>
2024-12-06 10:20:15 +0400

    added

--- oop3.py
+++ oop3.py
@@ -1,1 +1,7 @@
+import os
+# newfile = open("text.txt", "w")
+# newfile.write("Lewis Here learning how to code yooh! yooh!")
 
+file = open("text.txt", "r")
+print(file.read())
+#os.remove("text.txt")
